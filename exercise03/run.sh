fe3c5beff3a1d08ce26749c98727e290bc3c83f9
Tim Gross <tim@0x74696d.com>
2017-07-20 11:36:00 -0400

    render nginx template at start

--- exercise03/run.sh
+++ exercise03/run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+set -e
 
 # rewrite the template config for Nginx
 sed -i "s/PUBLIC_IP/${PUBLIC_IP}/" /site.conf
 sed -i "s/PORT/${PORT}/" /site.conf
+
+# render template once before starting up nginx
+consul-template \
+    -once \
+    -template "/site.conf:/etc/nginx/conf.d/site.conf" \
+    -template "/index.html:/usr/share/nginx/index.html"
 
 # run Nginx in the background
 nginx &
